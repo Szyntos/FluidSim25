91b672a46221fb061216edfd0c73636ac263f181
Szymon Nowak-Trzos <szymtos@gmail.com>
2023-11-03 22:27:56 +0100

    Added files for OpenGL

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <cmath>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
